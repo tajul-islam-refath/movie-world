b78c0e3bcf8f97f7aea774433c11c6f3201c3518
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-12-18 20:58:28 +0600

    upcomming movies section desing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/hero/Hero";
+import UpcommingMovies from "../components/upcomming-movies/UpcommingMovies";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <UpcommingMovies />
     </>
   );
 };
