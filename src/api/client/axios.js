5cc5c2766fa5c0bf319312d12baff4371765e03b
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-12-20 23:07:12 +0600

    update backend url

--- src/api/client/axios.js
+++ src/api/client/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api/v1";
+// const baseURL = "http://localhost:5000/api/v1";
+const baseURL = "https://movie-world-backend.vercel.app/api/v1";
 
 const axiosClient = axios.create({ baseURL });
 
